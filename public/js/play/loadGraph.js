c7a2279fd6c7584bccf66877fb22fb645f8a28ae
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-15 12:43:28 -0700

    added graph box with fixed graph image, ready to implement sounds

--- public/js/play/loadGraph.js
+++ public/js/play/loadGraph.js
@@ -1,7 +1,20 @@
 const loadGraph = (sound) => {
     console.log('sound is: ', sound);
 
-    document.body.style.background = 'url(./assets/images/graph-bg-cropped.png) repeat center center fixed';
+    const columnsEl = document.createElement('div');
+    columnsEl.classList.add('columns', 'is-vcentered');
+    columnsEl.style.paddingTop = '18px';
+    const column = document.createElement('div');
+    column.classList.add('column', 'is-10', 'is-offset-1');
+    const graphBox = document.createElement('div');
+    graphBox.classList.add('box');
+    graphBox.style.height = '95vh';
+    graphBox.style.background = 'url(./assets/images/graph-bg-cropped.png) repeat center center';
+
+    document.body.appendChild(columnsEl);
+    columnsEl.appendChild(column);
+    column.appendChild(graphBox)
+    
     //TODO: create graph background without image - use css instead.
 }
 
