7cd8cb67a313ce02140ab1d0c2b34e5d3c71383c
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-18 21:46:30 -0700

    adjusting env

--- netlify/functions/getSounds.js
+++ netlify/functions/getSounds.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const connect = mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true });
 const Sound = require('./models/Sound.js');
-console.log(process.env.MONGO_DB_URI);
+console.log('this is process env: ', process.env.MONGO_DB_URI);
 
 exports.handler = async (event, context) => {
   await connect;
