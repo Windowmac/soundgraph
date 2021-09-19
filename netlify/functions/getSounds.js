a06988a0237e96842668956d238340712c40b93b
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-18 21:09:40 -0700

    awaiting connect was not the timeout

--- netlify/functions/getSounds.js
+++ netlify/functions/getSounds.js
@@ -4,12 +4,12 @@ const Sound = require('./models/Sound.js');
 require('dotenv').config();
 
 exports.handler = async (event, context) => {
-  // await connect;
-  // const sounds = await Sound.find({}).catch((err) => {
-  //   console.log(err);
-  // });
+  await connect;
+  const sounds = await Sound.find({}).catch((err) => {
+    console.log(err);
+  });
 
-  // console.log(sounds);
+  console.log(sounds);
   return {
     statusCode: 200,
     body: JSON.stringify({hello: "world"})
