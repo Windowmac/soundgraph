bf191a5a3d52b478dc58d3ea881bdc2f8d12b856
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-18 21:33:02 -0700

    changed env reference to directory

--- netlify/functions/getSounds.js
+++ netlify/functions/getSounds.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '/.env') });
 const mongoose = require('mongoose');
 
 const connect = mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true });
