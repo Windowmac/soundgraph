95a2f8a54eef7a172f6f9989c2423066f8c44bd2
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-24 23:08:52 -0700

    made animation bubbles line up properly with fingers, and change colors depending on number of touches

--- public/assets/js/play/animateTouchBubble.js
+++ public/assets/js/play/animateTouchBubble.js
@@ -1,10 +1,19 @@
 const animateTouchBubble = (event) => {
     const bubbleNumber = 4;
     for(let i = 0; i < bubbleNumber; i++){
-        const x = event.touches ? event.touches[0].clientX + 'px' : event.clientX + 'px';
-        const y = event.touches ? event.touches[0].clientY + 'px' :event.clientY + 'px';
+        const x = event.touches ? event.touches[event.touches.length - 1].clientX + 'px' : event.clientX + 'px';
+        const y = event.touches ? event.touches[event.touches.length - 1].clientY + 'px' :event.clientY + 'px';
         const touchBubble = document.createElement('div');
         touchBubble.classList.add('touch-bubble');
+        if(event.touches.length > 1){
+            touchBubble.style.backgroundColor = "blue";
+        } 
+        if(event.touches.length > 2) {
+            touchBubble.style.backgroundColor = "red";
+        }
+        if(event.touches.length > 3) {
+            touchBubble.style.backgroundColor = "purple";
+        }
         touchBubble.style.animationDelay = i - 3 + 's';
         touchBubble.style.position = 'absolute';
         touchBubble.style.left = x;
