176094f64e43406c665e508e39696d2b56e29cb1
McKendree Strommer <mckendree.strommer@gmail.com>
2021-09-25 22:48:07 -0700

    fixed mouse click not playing sound because of animation bubble

--- public/assets/js/play/animateTouchBubble.js
+++ public/assets/js/play/animateTouchBubble.js
@@ -5,13 +5,13 @@ const animateTouchBubble = (event) => {
         const y = event.touches ? event.touches[event.touches.length - 1].clientY + 'px' :event.clientY + 'px';
         const touchBubble = document.createElement('div');
         touchBubble.classList.add('touch-bubble');
-        if(event.touches.length > 1){
+        if(event.touches && event.touches.length > 1){
             touchBubble.style.backgroundColor = "blue";
         } 
-        if(event.touches.length > 2) {
+        if(event.touches && event.touches.length > 2) {
             touchBubble.style.backgroundColor = "red";
         }
-        if(event.touches.length > 3) {
+        if(event.touches && event.touches.length > 3) {
             touchBubble.style.backgroundColor = "purple";
         }
         touchBubble.style.animationDelay = i - 3 + 's';
