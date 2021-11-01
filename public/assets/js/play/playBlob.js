f2371a986d285e1347ab9e1b1e9b8f729d32a29e
McKendree Strommer <mckendree.strommer@gmail.com>
2021-10-31 21:48:19 -0700

    added touch bubble removal and gain change on movement to playBlob

--- public/assets/js/play/playBlob.js
+++ public/assets/js/play/playBlob.js
@@ -17,12 +17,23 @@ const playBlob = async (event, audioCtx, audioSettings, graph, sound) => {
       const audioSourceNode = await audioCtx.createBufferSource();
       audioSourceNode.loop = true;
       audioSourceNode.buffer = audioSettings.decodedBuffer;
-      audioSourceNode.connect(audioSettings.primaryGainControl);
+      const blobGain = audioCtx.createGain();
+
+      blobGain.gain.setValueAtTime((CurX / audioSettings.WIDTH) * audioSettings.maxVol, audioCtx.currentTime);
+      audioSourceNode.connect(blobGain);
+
+      blobGain.connect(audioSettings.primaryGainControl)
       audioSourceNode.start();
       graph.addEventListener('mouseup', () => {
+        [...document.getElementsByClassName('touch-bubble')].forEach((bubble) => {
+            bubble.parentNode.removeChild(bubble);
+          });
         audioSourceNode.stop();
       });
       graph.addEventListener('touchend', () => {
+        [...document.getElementsByClassName('touch-bubble')].forEach((bubble) => {
+            bubble.parentNode.removeChild(bubble);
+          });
         audioSourceNode.stop();
       });
       return audioSourceNode;
